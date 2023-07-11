7875bbf8b901236de83813196d1e93f7308904fc
Andrey <andrey@pop-os.localdomain>
2023-07-11 13:59:56 +0200

    restart functionality

--- mainscreen.gd
+++ mainscreen.gd
@@ -12,8 +12,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if !$pacman.visible:
+	if not $pacman.visible:
 		$Victory.visible = true
+		$Button.visible = true
+		$Button.disabled = false
 	
 	
 	if Input.is_action_just_pressed("mouse_click"):
@@ -21,3 +23,15 @@ func _process(delta):
 		var target = targets[index]
 		target.global_position = get_global_mouse_position()
 		index = (index + 1) % targets.size()
+
+
+func _on_button_pressed() -> void:
+	$pacman.visible = true
+	$pacman.global_position = Vector2(625, 850)
+	$ghost.global_position = Vector2(450, 475)
+	$ghost2.global_position = Vector2(475, 475)
+	$ghost3.global_position = Vector2(500, 475)
+	$ghost4.global_position = Vector2(525, 475)
+	$Victory.visible = false
+	$Button.visible = false
+	$Button.disabled = true
